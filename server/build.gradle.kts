c1d787a713fc5dd4105c731bfa2ea302f9936afb
rugii913 <rugii913@gmail.com>
2024-01-14 21:32:37 +0900

    (빌드 실패 해결)build.gradle 파일 내용 root module에서 sub module로 옮김
    - 앞 커밋 후 빌드 돌려보니(프로젝트 루트에서 ./gradlew build 명령)
      -  빌드 실패하는 상태였음
    - 루트의 build.gradle.kts 파일 내용을 root module에서 sub module로 옮김
      - 루트의 build.gradle.kts 삭제
    - build 성공
      - /server/build/libs에서 war 파일 확인
      - /build/libs에서는 파일 확인하지 못함
      - 이 때 압축 해제해보면 모듈의 src/main/webapp에 넣어둔 html 파일을 war 파일 최상단에서 확인 가능
      - java 파일은 WEB-INF/classes/..(java 디렉토리 하위 경로 ex. hello/servlet/)../
        - 이 경로에서 컴파일된 class 파일로 있음을 확인 가능
      - library로 불러온 종속성 파일은 WEB-INF/lib 경로에서 확인 가능 
    - 이렇게 만들어진 war 파일을 Tomcat에 올려서 실행시키게 된다.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,7 +1,28 @@
+// 강의와는 다르게 Kotlin DSL을 사용해봄
 plugins {
     id("java")
+    id("war")
 }
+
+/*
+group = "hello" // 없어도 동작하는 듯함
+version = "1.0-SNAPSHOT" // 없어도 동작하는 듯함
+sourceCompatibility = "17" // 아래 java { } 블럭 참고
+*/
 
 repositories {
     mavenCentral()
+}
+
+dependencies {
+    // 서블릿
+    implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
