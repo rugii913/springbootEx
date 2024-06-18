2d4ac483c824072c2d945499ec734c4ecedb7ee7
rugii913 <rugii913@gmail.com>
2024-06-18 16:16:16 +0900

    docs: build.gradle에 war 파일 관련 주석 추가
    build.gradle에 war 파일 관련 주석 추가

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,12 +1,40 @@
 // 강의와는 다르게 Kotlin DSL을 사용해봄
 plugins {
     id("java")
-    id("war")
+    id("war") // 빌드 시 war 파일로 만들어주는 plugin
+    /*
+    * 생성된 war 파일은 jar -xvf 명령어로 압축 해제 가능
+    * - WAR(Web application ARchive) WAS에 배포할 때 사용하는 파일
+    *   - WAS 위에서 실행
+    *   - HTML 같은 정적 리소스와 클래스 파일을 모두 포함하여 JAR보다 구조가 복잡 → 특정한 구조를 지켜야 함
+    * - war 파일을 압축 해제하면 볼 수 있는 구조
+    *   - 루트에 src/main/webapp 디렉토리에 넣어둔 정적 리소스 파일들
+    *   - META-INF 디렉토리
+    *   - WEB-INF 디렉토리 → class 파일, 라이브러리, 설정 정보 위치
+    *     - classes 디렉토리(컴파일된 코드 - class 파일)
+    *     - lib 디렉토리(라이브러리 jar 파일들)
+    *     - web.xml (생략 가능) 웹 서버 배치 설정 파일
+    * - ./gradlew build로 생성된 war 파일을 [Tomcat 디렉토리]/webapps 디렉토리에 복사하고(기존 예제 파일은 모두 삭제)
+    *   - war 파일의 이름을 ROOT.war로 변경(ROOT는 모두 대문자)
+    *   - [Tomcat 디렉토리]/bin의 startup 파일을 실행
+    *   - cf. Windows 명령 프롬프트 한글 깨짐 해결 참고 → https://m.blog.naver.com/gyul611/222194321084
+    *   - cf. Tomcat 실행 시 알아서 war 압축 해제하여 실행
+    *   - cf. 작성한 클래스에 main()이 없어도 잘 실행됨을 알 수 있음 → main()은 Tomcat에서 갖고 있기 때문
+    * */
+    /*
+    * war 파일을 생성하고 Tomcat이 이를 실행하는 과정을 IDE에서 간편하게 해결하기(IntelliJ Ultimate 버전)
+    * - 메뉴 → Run → Edit Configurations(구성 편집) 화면 → + 클릭 → Tomcat Server 클릭
+    * - Server 탭 → Application server 옆 Configure 클릭 → 설치해둔 [Tomcat 디렉토리]로 설정
+    * - Deployment 탭 → + 클릭 → Artifact 클릭 → 배포할 war 파일 선택 후 OK 클릭
+    *   - Application context는 모두 지워서 공란으로 둠
+    *   - Before launch 부분에는 build 작업과 artifact 빌드 작업이 알아서 잘 들어가 있을 것
+    * - 이후에는 저장된 실행 구성 그대로 선택 후 IDE에서 실행하면 됨
+    * */
 }
 
 /*
-group = "hello" // 없어도 동작하는 듯함
-version = "1.0-SNAPSHOT" // 없어도 동작하는 듯함
+group = "hello" // 없어도 동작하는 듯함 → build까지도 별 문제 없음
+version = "1.0-SNAPSHOT" // 없어도 동작하는 듯함 → 문제는 없지만 build 시 생성되는 파일에 버전 정보 없음
 sourceCompatibility = "17" // 아래 java { } 블럭 참고
 */
 
