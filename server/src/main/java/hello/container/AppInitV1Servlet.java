64cabd5c888947aaeb06ee2b9af5dd40ee2ca4d2
rugii913 <rugii913@gmail.com>
2024-06-18 17:33:04 +0900

    docs: 애플리케이션 초기화 관련 설명 주석 추가, 기타 주석 정리
    애플리케이션 초기화 관련 설명 주석 추가, 기타 주석 정리

--- server/src/main/java/hello/container/AppInitV1Servlet.java
+++ server/src/main/java/hello/container/AppInitV1Servlet.java
@@ -10,10 +10,10 @@ import jakarta.servlet.ServletRegistration;
 public class AppInitV1Servlet implements AppInit {
 
     @Override
-    public void onStartup(ServletContext servletContext) {
+    public void onStartup(ServletContext servletContext) { // servlet context 초기화 중 이 onStartup() 호출을 통해 application 초기화를 진행하고, 이 과정에서 servlet을 등록
         System.out.println("AppInitV1Servlet.onStartup");
 
-        // 순수 서블릿 코드 등록 - 서블릿을 프로그래밍 방식으로 등록(TestServlet에서 사용한 @WebServlet 어노테이션 이용 방식 등록 x)
+        // 순수 servlet 코드 등록 - 서블릿을 프로그래밍 방식으로 등록(TestServlet에서 사용한 @WebServlet 어노테이션 이용 방식 등록 x)
         ServletRegistration.Dynamic helloServlet = servletContext.addServlet("helloServlet", new HelloServlet());
         // 위처럼 servletContext에 코드로 서블릿을 등록하면 Dynamic 타입을 반환 - 이 Dynamic에 mapping 정보를 추가
         helloServlet.addMapping("/hello-servlet");
