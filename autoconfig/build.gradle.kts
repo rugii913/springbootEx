6ac9fcfd48190b2d1555cf23d591e2e5e122c458
rugii913 <rugii913@gmail.com>
2024-06-20 12:18:00 +0900

    chore: autoconfig 모듈 생성, 종속성 설정 및 동작 확인
    autoconfig 모듈 생성, 종속성 설정 및 동작 확인
    - 종속성 Web, JDBC API, H2 Database, Lombok

---
+++ autoconfig/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "hello"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(annotationProcessor.get()) // dependencies에서 annotationProcessor에 지정된 라이브러리가 compileOnly 구성 시에도 포함되도록 하는 설정
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Initializr에서 Spring Web, JDBC API, H2 Database, Lombok을 선택하면 비슷하게 구성해줌
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // test 관련
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testCompileOnly("org.projectlombok:lombok") // test에서도 Lombok 사용
+    testAnnotationProcessor("org.projectlombok:lombok") // test에서도 Lombok 사용
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
