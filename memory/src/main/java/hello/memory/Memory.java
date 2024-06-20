cd10325f63fd185843acf8369d15077df28f43e5
rugii913 <rugii913@gmail.com>
2024-06-20 23:37:17 +0900

    feat: memory 모듈 생성, 앱의 JVM 메모리 사용량을 웹으로 응답하는 간단한 로직 작성
    memory 모듈 생성, 앱의 JVM 메모리 사용량을 웹으로 응답하는 간단한 로직 작성

---
+++ memory/src/main/java/hello/memory/Memory.java
@@ -0,0 +1,27 @@
+package hello.memory;
+
+public class Memory {
+    private final long used;
+    private final long max;
+
+    public Memory(long used, long max) {
+        this.used = used;
+        this.max = max;
+    }
+
+    public long getUsed() {
+        return used;
+    }
+
+    public long getMax() {
+        return max;
+    }
+
+    @Override
+    public String toString() {
+        return "Memory{" +
+                "used=" + used +
+                ", max=" + max +
+                '}';
+    }
+}
