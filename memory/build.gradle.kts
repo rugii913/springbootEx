24b3158ab5167bc2da106ecc5bf7ca15b4b25a89
rugii913 <rugii913@gmail.com>
2024-06-21 12:55:20 +0900

    test: 라이브러리화된 memory 모듈에 대한 테스트
    라이브러리화된 memory 모듈에 대한 테스트
    - Memory, MemoryFinder가 정상 동작하는지 확인

--- memory/build.gradle.kts
+++ memory/build.gradle.kts
@@ -41,4 +41,5 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:3.3.0")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.0")
 }
