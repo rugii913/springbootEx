229d31659bab2b76030c98112525e6b22938d78a
rugii913 <rugii913@gmail.com>
2024-06-21 12:03:21 +0900

    fix: Spring Boot의 @ConditionalOnProperty 활용
    Spring Boot의 @ConditionalOnProperty 활용
    - 앞에서 Condition을 구현하고, 이를 @Conditional에서 사용했던 것과 달리
      - @ConditionalOnProperty를 이용하여 훨씬 간편하게 작업 가능

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,13 +1,20 @@
 package hello.config;
 
-import memory.ConditionOnMemory;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
-@Conditional(ConditionOnMemory.class) // 명시된 Condition type의 matches()가 true를 반환할 때만 config가 등록
+@ConditionalOnProperty(name = "memory", havingValue = "on")
+/*
+* → JVM system property에서 값을 읽어와 조건적으로 판단하는 간편한 어노테이션
+*   의존 관계를 따라가보면 Condition의 구현 클래스인 OnPropertyCondition이 복잡한 로직으로 구현되어 있는 것 확인 가능
+* - 이 외에도 여러 @ConditionalOnXxx 어노테이션들이 → 주로 Spring Boot 자동 구성에 활용
+*   - https://docs.spring.io/spring-boot/reference/features/developing-auto-configuration.html
+*   - cf. @Conditional과 Condition은 spring-context 라이브러리에 있으나, 그 확장인 @ConditionalOnXxx는 spring-boot-autoconfigure 라이브러리
+* */
+//@Conditional(ConditionOnMemory.class) // 명시된 Condition type의 matches()가 true를 반환할 때만 config가 등록
 @Configuration
 public class MemoryConfig {
 
