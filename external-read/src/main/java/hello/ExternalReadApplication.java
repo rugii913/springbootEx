274cf7cb15f45acddde20c7955076b095834014b
rugii913 <rugii913@gmail.com>
2024-06-26 21:24:24 +0900

    docs: Spring이 지원하는 다양한 외부 설정 조회 방법 소개, Environment interface 주석
    Spring이 지원하는 다양한 외부 설정 조회 방법 소개, Environment interface 주석

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -9,4 +9,15 @@ public class ExternalReadApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExternalReadApplication.class, args);
     }
+    /*
+    * Spring이 지원하는 다양한 외부 설정 조회 방법과 Environment interface
+    * - Environment interface
+    *   - Environment type bean을 주입 받아 외부 설정 key=value 값을 조회할 수 있음
+    *     - 앞서 살펴보았듯 설정 데이터(application.properties), OS environment variables, Java system properties, commandline arguments
+    *   - Spring은 Environment bean을 활용하여 더 쉽게 외부 설정값을 읽는 방법들을 제공함
+    * - 다양한 외부 설정 조회 방법(Spring 지원)
+    *   - (1) Environment 직접 이용
+    *   - (2) @Value → 값 주입
+    *   - (3) @ConfigurationProperties → 타입 안전한 설정 속성
+    * */
 }
